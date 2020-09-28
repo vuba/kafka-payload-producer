634d2a8b44c54182c27052ad51085a8ca0d18ba1
vuba <vuba.git@gmail.com>
2020-09-28 21:56:33 +0200

    fix controller

--- src/main/java/com/vuba/kafkapayloadproducer/PayloadController.java
+++ src/main/java/com/vuba/kafkapayloadproducer/PayloadController.java
@@ -2,11 +2,10 @@ package com.vuba.kafkapayloadproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 public class PayloadController {
@@ -15,7 +14,7 @@ public class PayloadController {
     private PayloadService payloadService;
 
     @PostMapping(value = "/post/{kafkaTopic}")
-    public ResponseEntity postPayload(@PathParam("kafkaTopic") String kafkaTopic, @RequestBody String payload){
+    public ResponseEntity postPayload(@PathVariable("kafkaTopic") String kafkaTopic, @RequestBody String payload){
         payloadService.postPayload(kafkaTopic, payload);
         return ResponseEntity.ok().build();
     }
