771990b186156e363b73c1b25e700dd09d36f2a6
vuba <vuba.git@gmail.com>
2020-09-28 20:49:16 +0200

    initial commit

---
+++ src/main/java/com/vuba/kafkapayloadproducer/KafkaPayloadProducerApplication.java
@@ -0,0 +1,13 @@
+package com.vuba.kafkapayloadproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPayloadProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPayloadProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vuba/kafkapayloadproducer/KafkaPayloadProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuba.kafkapayloadproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPayloadProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
