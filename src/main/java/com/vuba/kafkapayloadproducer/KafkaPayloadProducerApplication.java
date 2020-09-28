ddecb13b65cb7522e05bc8615e204e194234a528
vuba <vuba.git@gmail.com>
2020-09-28 21:22:37 +0200

    enable swagger

--- src/main/java/com/vuba/kafkapayloadproducer/KafkaPayloadProducerApplication.java
+++ src/main/java/com/vuba/kafkapayloadproducer/KafkaPayloadProducerApplication.java
@@ -2,8 +2,12 @@ package com.vuba.kafkapayloadproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
+@ComponentScan
 public class KafkaPayloadProducerApplication {
 
 	public static void main(String[] args) {
